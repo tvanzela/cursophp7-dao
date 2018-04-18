30f0df4998bf7c91d2ec823126586f7d21366f9b
Tiago Vanzela <tvanzela@gmail.com>
2018-04-18 11:20:45 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -16,12 +16,23 @@ echo json_encode($lista);
 //carrega uma lisca com usuarios buscando pelo login
 $search = Usuario::search("oo");
 echo json_encode($search);
-*/
+
 
 //Carrega usuario usando login e senha
 $usuario = new Usuario();
 $usuario->login("root", "!@#$%");
 echo $usuario;
 
+//criando um novo usuario
+$aluno = new Usuario("aluno2", "AllUno");
+$aluno->insert();
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+$usuario->loadbyId(9);
+$usuario->update("Professor", "Pr0f3550%");
+
+echo $usuario;
 
  ?>
